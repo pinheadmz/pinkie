0a6f60a660fd18b062b738ffc0fdb5ff532de082
Matthew Zipkin <pinheadmz@gmail.com>
2020-08-08 18:23:35 -0400

    more iterations. move builld to js dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-bpkg --browser --umd --name HNSWallet --plugin uglify-es --output ./html/hnswallet-browser.js ./lib/hns-wallet.js
+bpkg --browser --umd --name HNSWallet --plugin uglify-es --output ./html/js/hnswallet-browser.js ./lib/hns-wallet.js
