93b1b924d83aac0e33f4caa4e88e012e28a299ad
Matthew Zipkin <pinheadmz@gmail.com>
2020-09-25 19:30:17 -0400

    pkg: split up build scripts

---
+++ scripts/build-dev.sh
@@ -0,0 +1,1 @@
+bpkg --browser --umd --name HNSWallet --output ./html/js/hnswallet-browser.js ./lib/hns-wallet.js
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,1 @@
-#bpkg --browser --umd --name HNSWallet --plugin uglify-es --output ./html/js/hnswallet-browser.js ./lib/hns-wallet.js
-bpkg --browser --umd --name HNSWallet --output ./html/js/hnswallet-browser.js ./lib/hns-wallet.js
+bpkg --browser --umd --name HNSWallet --plugin uglify-es --output ./html/js/hnswallet-browser.js ./lib/hns-wallet.js
